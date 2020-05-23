2ae0254f3101697bee64409fe9cd1ba0de7ff8cb
Three Fruits <65810911+ThreeFruitsDev@users.noreply.github.com>
2020-05-23 15:02:39 +0400

    Add files via upload

---
+++ GetData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetData : MonoBehaviour
+{
+    public void GetDataFrom(string url = "websitename/GetData.php")
+    {
+        StartCoroutine(GetDataIE(url));
+    }
+    private IEnumerator GetDataIE(string url)
+    {
+        WWWForm wWWForm = new WWWForm();
+        //if you want add some restriction
+        //wWWForm.AddField("page", 1);
+        //wWWForm.AddField("perpage", 10);
+        using (UnityWebRequest www = UnityWebRequest.Post(url, wWWForm))
+        {
+            www.certificateHandler = new BypassAllCertificate();
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                string _txt = www.downloadHandler.text;
+            }
+        }
+    }
+}
